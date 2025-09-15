7341cf185812d03f6f7c245a77aaab007b61dda2
shaikh rafe <skabdulrafe75@gmail.com>
2025-09-16 02:29:42 +0530

    type modul

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,14 +18,21 @@
 //   plugins: [],
 // }
 
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
+///** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+import { defineConfig } from "tailwindcss";
+
+export default defineConfig({
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: { extend: {} },
   plugins: [],
-}
+});
